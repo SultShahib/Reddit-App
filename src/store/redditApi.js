3e477321c9e8a4c5b3a8e777af2729d9d83d18bf
sultshahib1@gmail.com <89592909+SultShahib@users.noreply.github.com>
2022-07-07 13:51:44 +0800

    just checking and update

--- src/store/redditApi.js
+++ src/store/redditApi.js
@@ -3,7 +3,6 @@ const API = "https://www.reddit.com";
 const getSubredditPost = async (subreddit) => {
   const fetchPost = await fetch(`${API}${subreddit}`);
   const json = await fetchPost.json();
-  console.log("I am an absolute Conquerer");
 
   return json.data.children.map((post) => post.data);
 };
