c6f8714424449159cf7632561945131b16ab083f
sultshahib1@gmail.com <89592909+SultShahib@users.noreply.github.com>
2022-07-07 13:47:03 +0800

    Checking-makeover

--- src/store/redditApi.js
+++ src/store/redditApi.js
@@ -3,6 +3,11 @@ const API = "https://www.reddit.com";
 const getSubredditPost = async (subreddit) => {
   const fetchPost = await fetch(`${API}${subreddit}`);
   const json = await fetchPost.json();
+<<<<<<< Updated upstream
+=======
+  console.log("I am an absolute Conquerer");
+  console.log("No i am !!! absolute Conquerer");
+>>>>>>> Stashed changes
 
   return json.data.children.map((post) => post.data);
 };
